099fbea9a6c30f5c92957d02b04abeb9d3757688
stasek <stasek09@gmail.com>
2018-02-13 21:44:53 +0300

    Add target for class annotation

--- src/main/java/ru/svnik/tests/elements/Annotation.kt
+++ src/main/java/ru/svnik/tests/elements/Annotation.kt
@@ -2,6 +2,6 @@ package ru.svnik.tests.elements
 
 
 
-@Target(AnnotationTarget.FUNCTION)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class FakeRestServer(val port: Int,val  resourceFile: String)
